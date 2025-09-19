2ec4bf74f4896d4fed50a5e967b29e5c15f7a08f
ramez <ramezkhaled259@gmail.com>
2025-09-19 15:45:17 +0300

    The structure and some markup, styles

---
+++ guess.js
@@ -0,0 +1,7 @@
+// Setting the game name
+const gameName = "Guess The Word";
+document.title = gameName;
+document.querySelector("h1").textContent = gameName;
+document.querySelector(
+  "footer"
+).innerHTML = `${gameName} Game Created by Ramez`;
