1bb1f8f1c02665a29b6816bdd784db8a4c2f2405
ramez <ramezkhaled259@gmail.com>
2025-09-19 18:11:11 +0300

    adding some style and logic for generates the inputs

--- guess.js
+++ guess.js
@@ -5,3 +5,36 @@ document.querySelector("h1").textContent = gameName;
 document.querySelector(
   "footer"
 ).innerHTML = `${gameName} Game Created by Ramez`;
+
+// Setting game options
+let numOfTries = 6;
+let numOfLetters = 6;
+let currTry = 1;
+
+function generateInput() {
+  const inputsContainer = document.querySelector(".inputs");
+  //? Loop for creating Tries
+  for (let i = 1; i <= numOfTries; i++) {
+    const tryDiv = document.createElement("div");
+    tryDiv.classList.add(`try-${i}`);
+    tryDiv.innerHTML = `<span>Try ${i}</span>`;
+
+    if (i !== 1) tryDiv.classList.add("disabled-try");
+
+    //? Loop for creating Inputs for Letters
+    for (let j = 1; j <= numOfLetters; j++) {
+      const input = document.createElement("input");
+      input.type = "text";
+      input.id = `guess-${i}-letter-${j}`;
+      input.setAttribute("maxlength", "1");
+      tryDiv.appendChild(input);
+    }
+
+    inputsContainer.appendChild(tryDiv);
+  }
+  inputsContainer.children[0].children[1].focus();
+}
+
+window.onload = function () {
+  generateInput();
+};
